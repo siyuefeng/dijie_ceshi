d404b5701374e775004c26a9a2a82c69499003aa
boss chen <3274464668.com>
2018-07-12 15:32:49 +0800

    自动化测试入口文件

---
+++ main.py
@@ -0,0 +1,18 @@
+from runners.dijie_runner import DiJieRunner
+
+class Main(object):
+    """
+    自动化方案的唯一入口
+    """
+    @staticmethod
+    def run_dijie():
+        """
+        静态执行的方法
+        :param self:
+        :return:
+        """
+        print("start-test")
+        DiJieRunner().run_test()
+
+if __name__ == '__main__':
+    Main.run_dijie()
